af59f27dcd7ab2c7edde793fc5de58a6ed9d2f02
lenamatovic <lenamatovic2007@gmail.com>
2024-06-13 22:51:13 +0300

    zavrsila Engine klasu

--- oop-template/EngineUI.cs
+++ oop-template/EngineUI.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace oop_template
 {
@@ -15,6 +14,72 @@ namespace oop_template
         private HighScoreManager _highScoreManager;
         private GameDifficulty _difficulty;
         private TableLayoutPanel _tableLayoutPanel;
+
+        public EngineUI(int size, GameDifficulty difficulty)
+        {
+            _size = size;
+            _difficulty = difficulty;
+            _engine = new Engine(_size);
+            _highScoreManager = new HighScoreManager();
+            Text = "2048 Game";
+            Size = new Size(600, 600);
+            KeyDown += EngineUI_KeyDown;
+            _labels = new Label[_size, _size];
+            InitializeUI();
+        }
+
+        private void InitializeUI()
+        {
+            _tableLayoutPanel = new TableLayoutPanel
+            {
+                RowCount = _size,
+                ColumnCount = _size,
+                Dock = DockStyle.Fill,
+                CellBorderStyle = TableLayoutPanelCellBorderStyle.Single,
+                AutoSize = true,
+                AutoSizeMode = AutoSizeMode.GrowAndShrink
+            };
+
+            for (int i = 0; i < _size; i++)
+            {
+                _tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / _size));
+                _tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / _size));
+                for (int j = 0; j < _size; j++)
+                {
+                    var label = new Label
+                    {
+                        Dock = DockStyle.Fill,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Font = new Font("Arial", 24),
+                        BackColor = GetBlockColor(_engine.Board[i, j]),
+                        Text = _engine.Board[i, j] > 0 ? _engine.Board[i, j].ToString() : ""
+                    };
+                    _labels[i, j] = label;
+                    _tableLayoutPanel.Controls.Add(label, j, i);
+                }
+            }
+
+            Controls.Add(_tableLayoutPanel);
+        }
+
+        private Color GetBlockColor(int value)
+        {
+            switch (value)
+            {
+                case 2: return Color.LightYellow;
+                case 4: return Color.LightGoldenrodYellow;
+                case 8: return Color.Khaki;
+                case 16: return Color.Gold;
+                case 32: return Color.Orange;
+                case 64: return Color.OrangeRed;
+                case 128: return Color.LightSalmon;
+                case 256: return Color.Salmon;
+                case 512: return Color.DarkSalmon;
+                case 1024: return Color.Coral;
+                case 2048: return Color.Tomato;
+                default: return Color.LightGray;
+            }
+        }
 
         private void EngineUI_KeyDown(object sender, KeyEventArgs e)
         {
