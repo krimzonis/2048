1c7eb25a08406d01cd621ab04d50594af761fcb6
AndMihalich <122780316+AndMihalich@users.noreply.github.com>
2024-06-13 22:26:49 +0300

    dovrsen engine

--- oop-template/Engine.cs
+++ oop-template/Engine.cs
@@ -38,6 +38,55 @@ namespace oop_template
             OnBoardChanged();
         }
 
+        public bool Move(Direction direction)
+        {
+            SaveState();
+            bool moved = false;
+            bool merged = false;
+            switch (direction)
+            {
+                case Direction.Up: (moved, merged) = MoveUp(); break;
+                case Direction.Down: (moved, merged) = MoveDown(); break;
+                case Direction.Left: (moved, merged) = MoveLeft(); break;
+                case Direction.Right: (moved, merged) = MoveRight(); break;
+            }
+            if (moved)
+            {
+                if (merged)
+                {
+                    _soundEffectEngine.PlayMergeSound();
+                }
+                else
+                {
+                    _soundEffectEngine.PlayMoveSound();
+                }
+                AddRandomBlock();
+                OnBoardChanged();
+            }
+            return moved;
+        }
+
+        private void SaveState()
+        {
+            _history.Add((int[,])_board.Clone());
+            if (_history.Count > 4)
+            {
+                _history.RemoveAt(0);
+            }
+        }
+
+        public bool Undo()
+        {
+            if (_history.Count > 1)
+            {
+                _board = (int[,])_history[_history.Count - 1].Clone();
+                _history.RemoveAt(_history.Count - 1);
+                OnBoardChanged();
+                return true;
+            }
+            return false;
+        }
+
         private void AddRandomBlock()
         {
             List<Point> emptyPoints = new List<Point>();
@@ -57,6 +106,143 @@ namespace oop_template
                 Point p = emptyPoints[_random.Next(emptyPoints.Count)];
                 _board[p.X, p.Y] = _random.NextDouble() < 0.9 ? 2 : 4;
             }
+        }
+
+        private (bool moved, bool merged) MoveUp()
+        {
+            bool moved = false;
+            bool merged = false;
+            for (int j = 0; j < _size; j++)
+            {
+                for (int i = 1; i < _size; i++)
+                {
+                    if (_board[i, j] != 0)
+                    {
+                        int k = i;
+                        while (k > 0 && _board[k - 1, j] == 0)
+                        {
+                            _board[k - 1, j] = _board[k, j];
+                            _board[k, j] = 0;
+                            k--;
+                            moved = true;
+                        }
+                        if (k > 0 && _board[k - 1, j] == _board[k, j] && _board[k - 1, j] != 2048)
+                        {
+                            _board[k - 1, j] *= 2;
+                            _board[k, j] = 0;
+                            merged = true;
+                        }
+                    }
+                }
+            }
+            return (moved, merged);
+        }
+
+        private (bool moved, bool merged) MoveDown()
+        {
+            bool moved = false;
+            bool merged = false;
+            for (int j = 0; j < _size; j++)
+            {
+                for (int i = _size - 2; i >= 0; i--)
+                {
+                    if (_board[i, j] != 0)
+                    {
+                        int k = i;
+                        while (k < _size - 1 && _board[k + 1, j] == 0)
+                        {
+                            _board[k + 1, j] = _board[k, j];
+                            _board[k, j] = 0;
+                            k++;
+                            moved = true;
+                        }
+                        if (k < _size - 1 && _board[k + 1, j] == _board[k, j] && _board[k + 1, j] != 2048)
+                        {
+                            _board[k + 1, j] *= 2;
+                            _board[k, j] = 0;
+                            merged = true;
+                        }
+                    }
+                }
+            }
+            return (moved, merged);
+        }
+
+        private (bool moved, bool merged) MoveLeft()
+        {
+            bool moved = false;
+            bool merged = false;
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 1; j < _size; j++)
+                {
+                    if (_board[i, j] != 0)
+                    {
+                        int k = j;
+                        while (k > 0 && _board[i, k - 1] == 0)
+                        {
+                            _board[i, k - 1] = _board[i, k];
+                            _board[i, k] = 0;
+                            k--;
+                            moved = true;
+                        }
+                        if (k > 0 && _board[i, k - 1] == _board[i, k] && _board[i, k - 1] != 2048)
+                        {
+                            _board[i, k - 1] *= 2;
+                            _board[i, k] = 0;
+                            merged = true;
+                        }
+                    }
+                }
+            }
+            return (moved, merged);
+        }
+
+        private (bool moved, bool merged) MoveRight()
+        {
+            bool moved = false;
+            bool merged = false;
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = _size - 2; j >= 0; j--)
+                {
+                    if (_board[i, j] != 0)
+                    {
+                        int k = j;
+                        while (k < _size - 1 && _board[i, k + 1] == 0)
+                        {
+                            _board[i, k + 1] = _board[i, k];
+                            _board[i, k] = 0;
+                            k++;
+                            moved = true;
+                        }
+                        if (k < _size - 1 && _board[i, k + 1] == _board[i, k] && _board[i, k + 1] != 2048)
+                        {
+                            _board[i, k + 1] *= 2;
+                            _board[i, k] = 0;
+                            merged = true;
+                        }
+                    }
+                }
+            }
+            return (moved, merged);
+        }
+
+        public bool IsGameOver()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    if (_board[i, j] == 0)
+                        return false;
+                    if (i < _size - 1 && _board[i, j] == _board[i + 1, j])
+                        return false;
+                    if (j < _size - 1 && _board[i, j] == _board[i, j + 1])
+                        return false;
+                }
+            }
+            return true;
         }
 
         public void SaveHighScore(string playerName, GameDifficulty difficulty)
