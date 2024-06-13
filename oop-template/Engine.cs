f0f7def494cdbec6930d823a94a2719fecf0b3ec
krimzonis <aleksamilic3322@gmail.com>
2024-06-13 21:10:12 +0200

    delimican Engine i EngineUI

---
+++ oop-template/Engine.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_template
+{
+    public class Engine
+    {
+        public event Action BoardChanged;
+        private int[,] _board;
+        private List<int[,]> _history;
+        private Random _random;
+        private int _size;
+        private SoundEffectEngine _soundEffectEngine;
+        private HighScoreManager _highScoreManager;
+        public int[,] Board => _board;
+
+        public Engine(int size)
+        {
+            _size = size;
+            _board = new int[_size, _size];
+            _history = new List<int[,]>();
+            _random = new Random();
+            _soundEffectEngine = new SoundEffectEngine();
+            _highScoreManager = new HighScoreManager();
+            NewGame();
+        }
+
+        public void NewGame()
+        {
+            Array.Clear(_board, 0, _board.Length);
+            _history.Clear();
+            AddRandomBlock();
+            AddRandomBlock();
+            OnBoardChanged();
+        }
+
+        private void AddRandomBlock()
+        {
+            List<Point> emptyPoints = new List<Point>();
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    if (_board[i, j] == 0)
+                    {
+                        emptyPoints.Add(new Point(i, j));
+                    }
+                }
+            }
+
+            if (emptyPoints.Count > 0)
+            {
+                Point p = emptyPoints[_random.Next(emptyPoints.Count)];
+                _board[p.X, p.Y] = _random.NextDouble() < 0.9 ? 2 : 4;
+            }
+        }
+
+        public void SaveHighScore(string playerName, GameDifficulty difficulty)
+        {
+            int score = CalculateScore();
+            _highScoreManager.SaveHighScore(playerName, score, difficulty);
+        }
+
+        private int CalculateScore()
+        {
+            int score = 0;
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    score += _board[i, j];
+                }
+            }
+            return score;
+        }
+
+        private void OnBoardChanged()
+        {
+            BoardChanged?.Invoke();
+        }
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public enum GameDifficulty
+    {
+        Lako,
+        Srednje,
+        Tesko
+    }
+}
---
+++ oop-template/EngineUI.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_template
+{
+    public class EngineUI : Form
+    {
+        private Engine _engine;
+        private Label[,] _labels;
+        private int _size;
+        private HighScoreManager _highScoreManager;
+        private GameDifficulty _difficulty;
+        private TableLayoutPanel _tableLayoutPanel;
+
+        private void EngineUI_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool moved = false;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up: moved = _engine.Move(Direction.Up); break;
+                case Keys.Down: moved = _engine.Move(Direction.Down); break;
+                case Keys.Left: moved = _engine.Move(Direction.Left); break;
+                case Keys.Right: moved = _engine.Move(Direction.Right); break;
+                case Keys.Z: moved = _engine.Undo(); break;
+            }
+
+            if (moved)
+            {
+                UpdateUI();
+                if (_engine.IsGameOver())
+                {
+                    ShowGameOver();
+                }
+            }
+        }
+
+        private void UpdateUI()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    _labels[i, j].Text = _engine.Board[i, j] > 0 ? _engine.Board[i, j].ToString() : "";
+                    _labels[i, j].BackColor = GetBlockColor(_engine.Board[i, j]);
+                }
+            }
+        }
+
+        private void ShowGameOver()
+        {
+            string playerName = Microsoft.VisualBasic.Interaction.InputBox("Game Over! Napisi ime da bi ti se uneo highscore:");
+            _engine.SaveHighScore(playerName, _difficulty);
+            _highScoreManager.DisplayLeaderboard(_difficulty);
+            DialogResult result = MessageBox.Show("Zelis li zapoceti novu igru?", "Game Over", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                ChooseDifficulty();
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void ChooseDifficulty()
+        {
+            DifficultySelectionForm difficultyForm = new DifficultySelectionForm();
+            if (difficultyForm.ShowDialog() == DialogResult.OK)
+            {
+                int size = difficultyForm.SelectedSize;
+                GameDifficulty difficulty = difficultyForm.SelectedDifficulty;
+                StartGame(size, difficulty);
+            }
+        }
+
+        private void StartGame(int size, GameDifficulty difficulty)
+        {
+            _size = size;
+            _difficulty = difficulty;
+            Controls.Remove(_tableLayoutPanel);
+            _engine = new Engine(_size);
+            _labels = new Label[_size, _size];
+            InitializeUI();
+            UpdateUI();
+        }
+    }
+}
