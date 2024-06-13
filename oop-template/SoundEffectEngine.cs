066252f0bfa6c2620a140445b1f0a2c604887cb4
HerbikoSaMedom <lukastanisavljevic370@gmail.com>
2024-06-13 22:38:12 +0300

    Napravljena Sound Effect klassa

---
+++ oop-template/SoundEffectEngine.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_template
+{
+    public class SoundEffectEngine
+    {
+        private readonly SoundPlayer _moveSound;
+        private readonly SoundPlayer _mergeSound;
+
+        public SoundEffectEngine()
+        {
+            _moveSound = new SoundPlayer("whoosh.wav");
+            _mergeSound = new SoundPlayer("pop.wav"); 
+        }
+
+        public void PlayMoveSound()
+        {
+            _moveSound.Play();
+        }
+
+        public void PlayMergeSound()
+        {
+            _mergeSound.Play();
+        }
+    }
+
+    
+}
